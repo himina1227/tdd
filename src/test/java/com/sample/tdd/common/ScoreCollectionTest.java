000bfbb739b349a8a0da69d74a8124856ebed507
himina1227 <himina1227@gmail.com>
2023-03-26 19:29:31 +0900

    - ScoreCollectionTest 성공 테스트

--- src/test/java/com/sample/tdd/common/ScoreCollectionTest.java
+++ src/test/java/com/sample/tdd/common/ScoreCollectionTest.java
@@ -2,12 +2,23 @@ package com.sample.tdd.common;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ScoreCollectionTest {
 
     @Test
     public void test() {
-//        fail("실패 테스트");
+        // AAA Arrange - Act - Assert
+
+        // Arrange
+        ScoreCollection scoreCollection = new ScoreCollection();
+        scoreCollection.add(() -> 5);
+        scoreCollection.add(() -> 7);
+
+        // Act
+        int actualResult = scoreCollection.arithmeticMean();
+
+        // Assert
+        assertThat(actualResult == 6);
     }
 }
